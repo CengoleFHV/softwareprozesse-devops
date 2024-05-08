aadffac8d15e3d34809e236d32d1b801f685d7e2
CengoleFHV <cengizhan.aydin@students.fhv.at>
2024-05-08 15:35:39 +0200

    first first commit

---
+++ lib/src/main/java/org/example/Library.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class Library {
+
+    public boolean someLibraryMethod() {
+        return true;
+    }
+
+    public boolean isPCOn() {
+        return true;
+    }
+
+    public boolean guessNumber(int number) {
+        if (number == 5) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
